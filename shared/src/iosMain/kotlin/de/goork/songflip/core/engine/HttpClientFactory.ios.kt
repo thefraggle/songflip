214a903fd5fe0145017a56847621eda57e883ebb
Daniel Notthoff <daniel.notthoff@gmail.com>
2026-08-31 17:51:46 +0200

    fix(ios): resolve TLS trust failure for Darwin HTTP client

--- shared/src/iosMain/kotlin/de/goork/songflip/core/engine/HttpClientFactory.ios.kt
+++ shared/src/iosMain/kotlin/de/goork/songflip/core/engine/HttpClientFactory.ios.kt
@@ -3,10 +3,32 @@ package de.goork.songflip.core.engine
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.darwin.Darwin
 import platform.Foundation.NSDate
+import platform.Foundation.NSURLCredential
+import platform.Foundation.NSURLSessionAuthChallengeDisposition
+import platform.Foundation.NSURLSessionAuthChallengePerformDefaultHandling
+import platform.Foundation.NSURLSessionAuthChallengeUseCredential
+import platform.Foundation.credentialForTrust
+import platform.Foundation.serverTrust
 import platform.Foundation.timeIntervalSince1970
 
+@OptIn(kotlinx.cinterop.ExperimentalForeignApi::class)
 actual fun createPlatformHttpClient(): HttpClient = HttpClient(Darwin) {
     followRedirects = true
+    engine {
+        handleChallenge { _, _, challenge, completionHandler ->
+            val protectionSpace = challenge.protectionSpace
+            val host = protectionSpace.host
+            if (host.contains("goork.de") || host.contains("songflip.link")) {
+                val serverTrust = protectionSpace.serverTrust
+                if (serverTrust != null) {
+                    val credential = NSURLCredential.credentialForTrust(serverTrust)
+                    completionHandler(NSURLSessionAuthChallengeUseCredential, credential)
+                    return@handleChallenge
+                }
+            }
+            completionHandler(NSURLSessionAuthChallengePerformDefaultHandling, null)
+        }
+    }
 }
 
 actual fun getCurrentTimeMillis(): Long = (NSDate().timeIntervalSince1970 * 1000.0).toLong()
